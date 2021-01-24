519ad404d1057850d08c5a36440a630de6095c54
evgenii mishalov <forwow495800@ya.ru>
2021-01-25 00:32:02 +0100

    Fix currencies refetch time

--- src/services.config.ts
+++ src/services.config.ts
@@ -5,7 +5,7 @@ export const remoteBaseUrl = "http://localhost:5000";
 
 export const servicesProvider: IServicesProvider = new Remote();
 
-export const currenciesRefetchTime = 10000;
+export const currenciesRefetchTime = 15000;
 /**
  * можно вынести и в `.env`, но раз уж использую TSовский файл для конфига провайдера сервисов
  * то буду использовать и для определения раздичных констант
